a1539b19297dd076f15c8006433224e7eec97fd7
Ignacio Fontalvo <ignacio.fontalvo@globant.com>
2017-11-18 21:04:38 -0500

    set connection string to deploy in heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { mongoURI } = require('./../constants/constants');
 //credentials -> fontuser:Ign747212
 mongoose.Promise = global.Promise;
-// mongoose.connect(`mongodb://${process.env.mongoCredentials}${mongoURI}`);
+mongoose.connect(`mongodb://${process.env.mongoCredentials}${mongoURI}`);
 // mongoose.connect('mongodb://localhost:27017/point-fame');
-mongoose.connect(`mongodb://fontuser:Ign747212${mongoURI}`);
+// mongoose.connect(`mongodb://fontuser:Ign747212${mongoURI}`);
 mongoose.connection.on('error', () => {
 	throw new Error(`unable to connect to database: ${mongoURI}`);
 });
