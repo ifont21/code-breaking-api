f3f7e17807f08c6bbe73654d8fceaff25f08a2e4
Ignacio Fontalvo <ignacio.fontalvo@globant.com>
2017-11-18 21:27:13 -0500

    organizing mongoose file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const { mongoURI } = require('./../constants/constants');
-//credentials -> fontuser:Ign747212
+
 mongoose.Promise = global.Promise;
+
 mongoose.connect(`mongodb://${process.env.mongoCredentials}${mongoURI}`);
-// mongoose.connect('mongodb://localhost:27017/point-fame');
-// mongoose.connect(`mongodb://fontuser:Ign747212${mongoURI}`);
+
 mongoose.connection.on('error', () => {
 	throw new Error(`unable to connect to database: ${mongoURI}`);
 });
